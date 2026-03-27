442def9508d1c9c37c1f5763728002d6519a681e
Konstantin Tarmyshov <k.tarmyshov@googlemail.com>
2026-03-27 13:58:01 +0100

    issue with node coverage dump in teardown when running CI

--- tests/demo/e2e/global-teardown.mjs
+++ tests/demo/e2e/global-teardown.mjs
@@ -1,12 +1,17 @@
 import MCR from 'monocart-coverage-reports';
 
 export default async function globalTeardown() {
-	const client = await MCR.CDPClient({ port: 9230 });
-
 	try {
-		const coverageDir = await client.writeCoverage();
-		console.log('[coverage] dumped via CDP to:', coverageDir);
-	} finally {
-		await client.close();
+		const client = await MCR.CDPClient({ port: 9230 });
+
+		try {
+			const coverageDir = await client.writeCoverage();
+			console.log('[coverage] dumped via CDP to:', coverageDir);
+		} finally {
+			await client.close();
+		}
+	} catch (error) {
+		console.warn('[coverage] CDP coverage dump skipped');
+		console.warn(error);
 	}
 }
